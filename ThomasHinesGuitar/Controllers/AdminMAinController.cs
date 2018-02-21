abb30072ec2fe5fdf1fa4afaf265c6c5e8ea4439
Mark Boone <ab1994@mchsi.com>
2018-02-21 13:20:10 -0600

    Made a view.

---
+++ ThomasHinesGuitar/Controllers/AdminMAinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThomasHinesGuitar.Controllers
+{
+    public class AdminMainController : Controller
+    {
+        // GET: AdminMAin
+        public ActionResult AdminMainPage()
+        {
+            return View();
+        }
+    }
+}
